5434c9eb06cade138e876b34384ee17bfe0d85e2
Elai Shalev <eshalev@redhat.com>
2025-03-15 19:13:28 +0200

    get_all_places_in_radius now works

--- api/services.py
+++ api/services.py
@@ -20,7 +20,6 @@ def handel_address(street, house_number, radius):
   coordinates_list = get_coordinate(street=street, house_number=house_number)
   # {"0": ["32.062745", "34.770447"], "1": ["32.064218", "34.775547"]}
   x = json.loads(coordinates_list)
-  print(type(x))
   get_all_places_in_radius(x["0"], radius)
 
 
@@ -51,7 +50,7 @@ def get_coordinate(street, house_number):
     
     # comment this out if using API
     data = json.loads(get_mock_json())
-    
+
     places = {}
     i=0
     for place in data:
@@ -67,12 +66,19 @@ def get_coordinate(street, house_number):
 
 def get_mock_json():
    # So we don't abuse the 3rd party api
-   x = '''[{'place_id': 194986840, 'licence': 'Data © OpenStreetMap contributors, ODbL 1.0. http://osm.org/copyright', 'osm_type': 'node', 'osm_id': 2078982447, 'lat': '32.062745', 'lon': '34.770447', 'class': 'place', 'type': 'house', 'place_rank': 30, 'importance': 7.500038147550191e-05, 'addresstype': 'place', 'name': '', 'display_name': '12, שדרות רוטשילד, לב תל-אביב, תל־אביב–יפו, נפת תל אביב, מחוז תל אביב, 6688218, ישראל', 'boundingbox': ['32.0626950', '32.0627950', '34.7703970', '34.7704970']}, {'place_id': 195264854, 'licence': 'Data © OpenStreetMap contributors, ODbL 1.0. http://osm.org/copyright', 'osm_type': 'node', 'osm_id': 2078983104, 'lat': '32.064218', 'lon': '34.775547', 'class': 'place', 'type': 'house', 'place_rank': 30, 'importance': 7.500038147550191e-05, 'addresstype': 'place', 'name': '', 'display_name': '12א, שדרות רוטשילד, לב תל-אביב, תל־אביב–יפו, נפת תל אביב, מחוז תל אביב, 6578103, ישראל', 'boundingbox': ['32.0641680', '32.0642680', '34.7754970', '34.7755970']}]'''
-   x = x.replace("'", '"')
+   x = '''[{"place_id": "194986840", "licence": "Data © OpenStreetMap contributors, ODbL 1.0. http://osm.org/copyright", "osm_type": "node", "osm_id": 2078982447, "lat": "32.062745", "lon": "34.770447", "class": "place", "type": "house", "place_rank": 30, "importance": 7.500038147550191e-05, "addresstype": "place", "name": "", "display_name": "12, שדרות רוטשילד, לב תל-אביב, תל־אביב–יפו, נפת תל אביב, מחוז תל אביב, 6688218, ישראל", "boundingbox": ["32.0626950", "32.0627950", "34.7703970", "34.7704970"]}, {"place_id": 195264854, "licence": "Data © OpenStreetMap contributors, ODbL 1.0. http://osm.org/copyright", "osm_type": "node", "osm_id": 2078983104, "lat": "32.064218", "lon": "34.775547", "class": "place", "type": "house", "place_rank": 30, "importance": 7.500038147550191e-05, "addresstype": "place", "name": "", "display_name": "12א, שדרות רוטשילד, לב תל-אביב, תל־אביב–יפו, נפת תל אביב, מחוז תל אביב, 6578103, ישראל", "boundingbox": ["32.0641680", "32.0642680", "34.7754970", "34.7755970"]}]'''
    return x
    
 def get_all_places_in_radius(coordinate, radius):
   url = "https://gisn.tel-aviv.gov.il/arcgis/rest/services/WM/IView2WM/MapServer/772/query?"
+
+  print(coordinate)
+  geometry = {
+    "x": float(coordinate[1]),
+    "y": float(coordinate[0])
+  }
+
+  out_fields = ",".join(["addresses", "building_stage","sw_tama_38"])
 
   params = {
     "where": "1=1",
@@ -80,19 +86,14 @@ def get_all_places_in_radius(coordinate, radius):
     "objectIds": "",
     "time": "",
     "timeRelation": "esriTimeRelationOverlaps",
-    "geometry" : json.dumps({
-        'x': float(coordinate[0]),
-        'y': float(coordinate[1]),
-    }), "spatialReference":json.dumps({
-        "wkid":4326
-    }),
+    "geometry" : json.dumps(geometry),
     "geometryType": "esriGeometryPoint",
     "inSR": "4326",
     "spatialRel": "esriSpatialRelIntersects",
-    "distance": "",
+    "distance": str(radius),
     "units": "esriSRUnit_Meter",
     "relationParam": "",
-    "outFields": "addresses",
+    "outFields": out_fields,
     "returnGeometry": "false",
     "returnTrueCurves": "false",
     "maxAllowableOffset": "",
@@ -121,26 +122,28 @@ def get_all_places_in_radius(coordinate, radius):
     "f": "pjson",
   }
 
-
-  #params["geometry"] = f'{{"x": {coordinate[0]} ,"y": {coordinate[1]} , "spatialReference": {{"wkid": 4326}}}}'
-  #params['geometry'] = quote(params['geometry'])
-  params["distance"] = f'{radius}'
-
 # Define headers
   headers = {
       "Accept": "application/json",
       "Accept-Language": "en-US,en;q=0.9",
   }
 
+
   try:
-    # TODO - The paramaters aren't getting created correctly, so the request returns empty / all the db is returned
-    print(url)
-    print(params)
-    response = requests.get(url, params=params, headers=headers)
-    response.raise_for_status()
-    data = response.json()
-    print(data['features'])
-    return "aa"
+    # Uncomment to actually Query the GIST API. 
+    #response = requests.get(url, params=params, headers=headers)
+    # print(response.url)  # Debugging: Check the final URL
+    # print(response.json())  # Check the response content
+
+    data = '''[{'attributes': {'addresses': 'שדרות רוטשילד 16', 'building_stage': 'קיים היתר', 'sw_tama_38': 'לא'}}, {'attributes': {'addresses': 'שדרות רוטשילד 16', 'building_stage': 'קיים היתר', 'sw_tama_38': 'לא'}}, {'attributes': {'addresses': 'שדרות רוטשילד 16', 'building_stage': 'קיים היתר', 'sw_tama_38': 'לא'}}, {'attributes': {'addresses': 'שדרות רוטשילד 16', 'building_stage': 'קיים היתר', 'sw_tama_38': 'לא'}}, {'attributes': {'addresses': 'הרצל 5, שדרות רוטשילד 9', 'building_stage': 'בבניה', 'sw_tama_38': 'לא'}}, {'attributes': {'addresses': 'הרצל 7, שדרות רוטשילד 10, שדרות רוטשילד 10א', 'building_stage': 'בתהליך היתר', 'sw_tama_38': 'לא'}}, {'attributes': {'addresses': 'הרצל 7, שדרות רוטשילד 10, שדרות רוטשילד 10א', 'building_stage': 'בתהליך היתר', 'sw_tama_38': 'לא'}}, {'attributes': {'addresses': 'הרצל 7, שדרות רוטשילד 10, שדרות רוטשילד 10א', 'building_stage': 'בתהליך היתר', 'sw_tama_38': 'לא'}}, {'attributes': {'addresses': 'הרצל 7, שדרות רוטשילד 10, שדרות רוטשילד 10א', 'building_stage': 'בתהליך היתר', 'sw_tama_38': 'לא'}}, {'attributes': {'addresses': 'הרצל 7, שדרות רוטשילד 10, שדרות רוטשילד 10א', 'building_stage': 'בתהליך היתר', 'sw_tama_38': 'לא'}}, {'attributes': {'addresses': 'הרצל 7, שדרות רוטשילד 10, שדרות רוטשילד 10א', 'building_stage': 'בתהליך היתר', 'sw_tama_38': 'לא'}}, {'attributes': {'addresses': 'הרצל 7, שדרות רוטשילד 10, שדרות רוטשילד 10א', 'building_stage': 'בתהליך היתר', 'sw_tama_38': 'לא'}}, {'attributes': {'addresses': 'הרצל 7, שדרות רוטשילד 10, שדרות רוטשילד 10א', 'building_stage': 'בתהליך היתר', 'sw_tama_38': 'לא'}}]
+'''
+
+    #response.raise_for_status()
+    # data = response.json()
+    # print(data['features'])
+    # return data['features']
+    print(data)
+    return data
   except requests.RequestException as e:
       return JsonResponse({"error": "AAA"+str(e)}, status=500, safe=False)
 
