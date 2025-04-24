45a4a326f34025a889bf730847fa6f3f25faba35
Elai Shalev <eshalev@redhat.com>
2025-04-24 11:32:44 +0300

    changed module name

--- tamaod/settings.py
+++ tamaod/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'TamaOd.urls'
+ROOT_URLCONF = 'tamaod.urls'
 
 TEMPLATES = [
     {
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'TamaOd.wsgi.application'
+WSGI_APPLICATION = 'tamaod.wsgi.application'
 
 
 # Database
